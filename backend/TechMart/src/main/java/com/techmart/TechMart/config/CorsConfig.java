7314f3933b9d511f940d799f1994bcc9f7906583
Pfisthis2004 <159265292+Pfisthis2004@users.noreply.github.com>
2026-01-06 21:14:33 +0700

    cau hinh Cors

--- backend/TechMart/src/main/java/com/techmart/TechMart/config/CorsConfig.java
+++ backend/TechMart/src/main/java/com/techmart/TechMart/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000",
+                        "https://techshop-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
