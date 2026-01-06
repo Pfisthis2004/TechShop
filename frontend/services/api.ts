1abc27c40f6129285ad66dce534a52c6c209da2e
Pfisthis2004 <159265292+Pfisthis2004@users.noreply.github.com>
2026-01-06 21:48:26 +0700

    Cập nhật baseURL axios sang Render

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -5,7 +5,7 @@ import { Product, Order, Category, User } from '../types';
 
 // Bước 1: Khởi tạo gọn nhẹ
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { "ngrok-skip-browser-warning": "true" }
 });
 /*
